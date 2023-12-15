b1321976be03e5347e488b1b5183ddc8b84662da
joerivanarkel <Joeri@vanarkel.net>
2023-12-15 23:42:12 +0100

    tailwind and npm set up

--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Presentation;
+using Presentation.Shared;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
